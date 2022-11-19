b880095b82910c61a55e55b45263e5f59931d252
Manikandan2397 <mainaravind2397@gmail.com>
2022-11-19 15:50:43 +0530

    detail

---
+++ src/test/collection/Test.java
@@ -0,0 +1,38 @@
+package test.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+	public static void main(String[] args) {
+		//list
+		
+		List a = new ArrayList();
+		a.add(10);
+		a.add(20);
+		a.add("deepak");
+		a.add(true);
+		
+		List <Integer> l = new ArrayList<Integer>();
+		l.add(20);
+		l.add(30);
+		
+		
+		int size = l.size();
+		System.out.println(size);
+		
+		Integer integer = l.get(0);
+		System.out.println(integer);
+		int indexOf = l.indexOf(20);
+		System.out.println(indexOf);
+		
+		
+		l.addAll(a);
+		System.out.println(l);
+		
+		
+		
+		
+}
+
+}
